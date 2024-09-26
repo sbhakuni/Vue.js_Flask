c59ece7d32fdcd8d5d5257e4ecd4c0eaab0414d3
sbhakuni <bhakunishantanu@gmail.com>
2024-09-25 22:47:50 -0700

    signup component and db setup

---
+++ resto-project/src/components/SignUp.vue
@@ -0,0 +1,37 @@
+<template>
+    <img class="logo" src="../assets/resto-logo.jpg" />
+    <h1>Sign Up</h1>
+    <div class="register">
+      <input type="text" placeholder="Enter Name" />
+      <input type="text" placeholder="Enter Email" />
+      <input type="password" placeholder="Enter Password" />
+      <button>Sign Up</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SignUp'
+  }
+  </script>
+  
+  <style>
+  .logo {
+    width: 100px;
+  }
+  .register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-left: auto;
+  margin-right: auto;
+  border: 1px solid skyblue;
+}
+
+.register button {
+  /* Add styles for the button here */
+}
+  </style>
+  
